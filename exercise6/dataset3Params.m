01e30a14e9bf818b1d2a9e217342646b6aeb0e3e
fdc <fdcancellara@gmail.com>
2017-02-22 11:05:40 -0300

    Added function to search for the best C and sigma parameters for the SVM.

--- exercise6/dataset3Params.m
+++ exercise6/dataset3Params.m
@@ -23,9 +23,29 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+cVector = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+sigmaVector = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+
+lowestError = intmax;
 
 
-
+for cIndex = 1:length(cVector)
+  for sigmaIndex = 1:length(sigmaVector)
+  
+    model = svmTrain(X, y, cVector(cIndex), @(x1, x2) gaussianKernel(x1, x2, sigmaVector(sigmaIndex)));
+    
+    predictions = svmPredict(model, Xval);
+    
+    error = mean(double(predictions ~= yval));
+    
+    if (error < lowestError)
+      lowestError = error;
+      C = cVector(cIndex);
+      sigma = sigmaVector(sigmaIndex);
+    end
+    
+  end
+end
 
 
 
